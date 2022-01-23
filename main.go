fcd712a9f2fed640568f0316b0e580c6deaab88e
apple-yagi <applesryu@gmail.com>
2022-01-23 17:10:06 +0900

    Change function order

--- main.go
+++ main.go
@@ -7,18 +7,6 @@ import (
 	"github.com/apple-yagi/congo/handler"
 )
 
-func makeRequest(url string, response *http.Response) handler.GenericFunction {
-	return func() error {
-		resp, err := http.Get(url)
-		if err != nil {
-			return err
-		}
-		fmt.Println(resp)
-		response = resp
-		return nil
-	}
-}
-
 func main() {
 	var resp1 http.Response
 	var resp2 http.Response
@@ -29,4 +17,17 @@ func main() {
 		makeRequest("https://example3.com/", &resp3),
 	}
 	fmt.Println(handler.RunAsyncAllowErrors(funcs...))
+	fmt.Println("After")
+}
+
+func makeRequest(url string, response *http.Response) handler.GenericFunction {
+	return func() error {
+		resp, err := http.Get(url)
+		if err != nil {
+			return err
+		}
+		fmt.Println(resp)
+		response = resp
+		return nil
+	}
 }
